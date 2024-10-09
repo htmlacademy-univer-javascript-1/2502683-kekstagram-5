65b0201757b35bb18cc1913c205b42d894be49c7
xyulo <jeka.meka.1998@mail.ru>
2024-10-10 03:35:32 +0500

    .gitignore

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,22 @@
 function isStringValid(string, maxLength) {
-    if (string.length <= maxLength) 
+    if(string.length <= maxLength) 
         return true
     else
         return false;
+}
+
+function isPalindrome(string) {
+    const normalizedString = string
+      .toLowerCase()
+      .replace(/[^a-zа-яё0-9]/g, '');
+  
+    return normalizedString === normalizedString.split('').reverse().join('');
+  }
+
+function имяФункции(inputData) {
+    if (typeof inputData === 'number') {
+        inputData = Math.abs(inputData).toString();
+    }
+    let digits = inputData.replace(/\D/g, '');
+    return digits.length > 0 ? parseInt(digits, 10) : NaN;
 }
