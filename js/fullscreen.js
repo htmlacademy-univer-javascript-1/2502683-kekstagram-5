9093d63f65e1dabfb03beb20af0fc98669fa3c01
xyulo <jeka.meka.1998@mail.ru>
2024-11-25 21:06:10 +0500

    .gitignore

---
+++ js/fullscreen.js
@@ -0,0 +1,58 @@
+const body = document.body;
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const socialComments = bigPicture.querySelector('.social__comments');
+const socialCaption = bigPicture.querySelector('.social__caption');
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+const commentCountBlock = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+const openFullscreen = (photoData) => {
+  bigPictureImg.src = photoData.url;
+  likesCount.textContent = photoData.likes;
+  commentsCount.textContent = photoData.comments.length;
+  socialCaption.textContent = photoData.description;
+
+  socialComments.innerHTML = '';
+  
+  photoData.comments.forEach((comment) => {
+    const commentElement = document.createElement('li');
+    commentElement.classList.add('social__comment');
+    commentElement.innerHTML = `
+      <img
+        class="social__picture"
+        src="${comment.avatar}"
+        alt="${comment.name}"
+        width="35" height="35">
+      <p class="social__text">${comment.message}</p>
+    `;
+    socialComments.appendChild(commentElement);
+  });
+
+  commentCountBlock.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onEscKeyPress);
+};
+
+const closeFullscreen = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeyPress);
+};
+
+const onEscKeyPress = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeFullscreen();
+  }
+};
+
+closeButton.addEventListener('click', closeFullscreen);
+
+export { openFullscreen };
---
+++ js/thumbnails.js
@@ -0,0 +1,28 @@
+import { generatePhotoData } from './photos.js';
+import { openFullscreen } from './fullscreen.js';
+
+const renderThumbnails = () => {
+  const photos = generatePhotoData();
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const picturesContainer = document.querySelector('.pictures');
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__img').alt = photo.description;
+    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+    pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+
+    // Добавляем обработчик клика для открытия полноразмерного изображения
+    pictureElement.addEventListener('click', () => {
+      openFullscreen(photo);
+    });
+
+    fragment.appendChild(pictureElement);
+  });
+
+  picturesContainer.appendChild(fragment);
+};
+
+export { renderThumbnails };
