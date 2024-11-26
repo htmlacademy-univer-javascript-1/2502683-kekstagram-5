df4802295ce3edf22b0f4275de405a2c697bfaef
xyulo <jeka.meka.1998@mail.ru>
2024-11-26 21:49:34 +0500

    .gitignore

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { generatePhotoData } from './photos.js';
+import { renderThumbnails } from './thumbnails.js';
 
 const photoData = generatePhotoData();
 console.log(photoData);
+
+renderThumbnails();
