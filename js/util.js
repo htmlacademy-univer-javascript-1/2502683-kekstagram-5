3f7d5bf26f49377cacc911e663cd0ae2f2f8160d
Lazyazazaz <152683355+Lazyazazaz@users.noreply.github.com>
2024-11-26 21:28:30 +0500

    Delete js/util.js

--- js/util.js
+++
@@ -1,7 +0,0 @@
-export function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export function getRandomElement(arr) {
-    return arr[getRandomInt(0, arr.length - 1)];
-}
