d23284ac0bfb638a57d7e7010333e31bcf9c88ba
Keks <keks@htmlacademy.ru>
2024-12-09 10:37:39 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/function.js
@@ -0,0 +1,19 @@
+function parseTime(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+}
+
+function isMeetingInWorkHours(startWork, endWork, startMeeting, duration) {
+    const startWorkMinutes = parseTime(startWork);
+    const endWorkMinutes = parseTime(endWork);
+    const startMeetingMinutes = parseTime(startMeeting);
+    const endMeetingMinutes = startMeetingMinutes + duration;
+
+    return startMeetingMinutes >= startWorkMinutes && endMeetingMinutes <= endWorkMinutes;
+}
+
+console.log(isMeetingInWorkHours('08:00', '17:30', '14:00', 90)); // true
+console.log(isMeetingInWorkHours('8:0', '10:0', '8:0', 120));     // true
+console.log(isMeetingInWorkHours('08:00', '14:30', '14:00', 90)); // false
+console.log(isMeetingInWorkHours('14:00', '17:30', '08:0', 90));  // false
+console.log(isMeetingInWorkHours('8:00', '17:30', '08:00', 900)); // false
---
+++ 17/js/main.js
@@ -0,0 +1,9 @@
+import { generatePhotoData } from './photos.js';
+import { renderThumbnails } from './thumbnails.js';
+import { renderThumbnails } from './render-thumbnails.js'
+
+const photoData = generatePhotoData();
+console.log(photoData);
+
+renderThumbnails();
+renderThumbnails();
